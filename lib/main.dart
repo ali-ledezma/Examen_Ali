7c97cf9ec99295c71ff9fbdc28aa422fd42eedce
DIEGO_ALI_LEDEZMA_CARBAJAL <a.22308051281229@cbtis128.edu.mx>
2025-03-11 15:18:59 +0000

    grgr

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,29 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Ledezma - 1229'), // Título con tu nombre y código
+          backgroundColor: Colors.green,
+        ),
+        body: ListView.builder(
+          itemCount: 8,
+          itemBuilder: (context, index) {
+            return ListTile(
+              leading: Icon(Icons.list, color: Colors.blue),
+              title: Text('List item $index',
+                  style: TextStyle(color: Color(0xffea2d2d))),
+              trailing: Text('GFG', style: TextStyle(color: Colors.red)),
+            );
+          },
         ),
       ),
     );
